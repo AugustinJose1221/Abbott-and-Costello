4de01efbfc66191fd7cc9012463b9c797abc923b
AugustinJose1221 <augustinjose1221@gmail.com>
2020-06-03 17:53:52 +0530

    Added RSA encrypted communication

--- Test2/client.py
+++ Test2/client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jun  3 13:45:40 2020
+Created on Wed Jun  3 13:45:16 2020
 
 @author: augustinjose
 """
@@ -9,133 +9,51 @@ Created on Wed Jun  3 13:45:40 2020
 
 import socket
 import os
-import signal
 import threading
 import hashlib
-import fcntl
-import struct
 from Crypto import Random
 import Crypto.Cipher.AES as AES
 from Crypto.PublicKey import RSA
-
-
-def get_ip_address(ifname):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
-        0x8915,
-        struct.pack('256s', ifname[:15])
-    )[20:24])
+import signal
 
 
 def RemovePadding(s):
-    return s.replace('`','')
+    return s.replace(b'`',b'')
 
 
 def Padding(s):
     return s + ((16 - len(s) % 16) * '`')
 
 
-def ConnectionSetup():
+def ReceiveMessage():
     while True:
-        if check is True:
-            client, address = server.accept()
-            print("\n[!] One client is trying to connect...")
-            # get client public key and the hash of it
-            clientPH = client.recv(2048)
-            split = clientPH.split(":")
-            tmpClientPublic = split[0]
-            clientPublicHash = split[1]
-            print("\n[!] Anonymous client's public key\n")
-            print(tmpClientPublic)
-            tmpClientPublic = tmpClientPublic.replace("\r\n", '')
-            clientPublicHash = clientPublicHash.replace("\r\n", '')
-            tmpHashObject = hashlib.md5(tmpClientPublic)
-            tmpHash = tmpHashObject.hexdigest()
-
-            if tmpHash == clientPublicHash:
-                # sending public key,encrypted eight byte ,hash of eight byte and server public key hash
-                print("\n[!] Anonymous client's public key and public key hash matched\n")
-                clientPublic = RSA.importKey(tmpClientPublic)
-                fSend = eightByte + ":" + session + ":" + my_hash_public
-                fSend = clientPublic.encrypt(fSend, None)
-                client.send(str(fSend) + ":" + public)
-
-                clientPH = client.recv(2048)
-                if clientPH != "":
-                    clientPH = RSA.importKey(private).decrypt(eval(clientPH.decode('utf-8')))
-                    print("\n[!] Matching session key\n")
-                    if clientPH == eightByte:
-                        # creating 128 bits key with 16 bytes
-                        print("\n[!] Creating AES key\n")
-                        key_128 = eightByte + eightByte[::-1]
-                        AESKey = AES.new(key_128, AES.MODE_CBC,IV=key_128)
-                        clientMsg = AESKey.encrypt(Padding(FLAG_READY))
-                        client.send(clientMsg)
-                        print("\n[!] Waiting for client's name\n")
-                        # client name
-                        clientMsg = client.recv(2048)
-                        CONNECTION_LIST.append((clientMsg, client))
-                        print("\n"+clientMsg+" IS CONNECTED")
-                        threading_client = threading.Thread(target=broadcast_usr,args=[clientMsg,client,AESKey])
-                        threading_client.start()
-                        threading_message = threading.Thread(target=send_message,args=[client,AESKey])
-                        threading_message.start()
-                    else:
-                        print("\nSession key from client does not match")
-            else:
-                print("\nPublic key and public hash doesn't match")
-                client.close()
+        emsg = server.recv(1024)
+        msg = RemovePadding(AESKey.decrypt(emsg))
+        if msg == FLAG_QUIT:
+            print("\n[!] Server was shutdown by admin")
+            os.kill(os.getpid(), signal.SIGKILL)
+        else:
+            print("\n[!] Server's encrypted message \n",  emsg)
+            print("\n[!] SERVER SAID : ", msg)
 
 
-def send_message(socketClient,AESk):
+def SendMessage():
     while True:
-        msg = input("\n[>] ENTER YOUR MESSAGE : ")
-        en = AESk.encrypt(Padding(msg))
-        socketClient.send(str(en))
+        msg = input("[>] YOUR MESSAGE : ")
+        en = AESKey.encrypt(Padding(msg))
+        server.send(en)
         if msg == FLAG_QUIT:
             os.kill(os.getpid(), signal.SIGKILL)
         else:
-            print("\n[!] Your encrypted message \n" + en)
-
-
-def broadcast_usr(uname, socketClient,AESk):
-    while True:
-        try:
-            data = socketClient.recv(1024)
-            en = data
-            if data:
-                data = RemovePadding(AESk.decrypt(data))
-                if data == FLAG_QUIT:
-                    print("\n"+uname+" left the conversation")
-                else:
-                    b_usr(socketClient, uname, data)
-                    print("\n[!] ", uname, " SAID : ", data)
-                    print("\n[!] Client's encrypted message\n" + en)
-        except Exception as x:
-            print(x.message)
-            break
-
-
-def b_usr(cs_sock, sen_name, msg):
-    for client in CONNECTION_LIST:
-        if client[1] != cs_sock:
-            client[1].send(sen_name)
-            client[1].send(msg)
+            print("\n[!] Your encrypted message \n ", en)
 
 
 if __name__ == "__main__":
-    # objects
-    host = ""
-    port = 0
+    #objects
     server = ""
     AESKey = ""
-    CONNECTION_LIST = []
     FLAG_READY = "Ready"
     FLAG_QUIT = "quit"
-    YES = "1"
-    NO = "2"
-
     # 10.1.236.227
     # public key and private key
     random = Random.new().read
@@ -146,55 +64,84 @@ if __name__ == "__main__":
     tmpPub = hashlib.md5(public)
     my_hash_public = tmpPub.hexdigest()
 
-    eightByte = os.urandom(8)
-    sess = hashlib.md5(eightByte)
-    session = sess.hexdigest()
+    #print(public)
+    #print("\n",private)
 
-    with open('private.txt', 'w'):
+    host = input("Host : ")
+    port = int(input("Port : "))
+#    host = "127.0.0.1"
+#    port = 5599
+
+    with open('private.txt', 'wb'):
         pass
-    with open('public.txt', 'w'):
+    with open('public.txt', 'wb'):
         pass
 
     try:
-        file = open('private.txt', 'w')
+        file = open('private.txt', 'wb')
         file.write(private)
         file.close()
-
-        file = open('public.txt', 'w')
+    
+        file = open('public.txt', 'wb')
         file.write(public)
         file.close()
     except BaseException:
         print("Key storing in failed")
 
     check = False
-    print("[1] Auto connect by with broadcast IP & PORT\n[2] Manually enter IP & PORT\n")
-    ask = input("[>] ")
-    if ask == YES:
-        host = get_ip_address('wlp2s0')
-        port = 8080
-    elif ask == NO:
-        host = input("Host : ")
-        port = int(input("Port : "))
-    else:
-        print("[!] Invalid selection")
-        os.kill(os.getpid(), signal.SIGKILL)
 
-    print("\n",public,"\n\n",private)
-    print("\n[!] Eight byte session key in hash\n")
-    print(session)
-    print("\n[!] Server IP "+host+" & PORT "+str(port))
+    try:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.connect((host, port))
+        check = True
+    except BaseException:
+        print("\n[!] Check Server Address or Port")
 
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind((host, port))
-    server.listen(1)
-    print("\n[!] Server Connection Successful")
-    check = True
-    # accept clients
-    threading_accept = threading.Thread(target=ConnectionSetup)
-    threading_accept.start()
+    if check is True:
+        print("\n[!] Connection Successful")
+        server.send(public + b":" + bytes(my_hash_public, "utf-8"))
+        # receive server public key,hash of public,eight byte and hash of eight byte
+        fGet = server.recv(4072)
+        split = fGet.split(b"\:")
+        print(fGet)
+        toDecrypt = split[0]
+        serverPublic = split[1]
+        print("\n[!] Server's public key\n")
+        decrypted = RSA.importKey(private).decrypt(toDecrypt.replace(b"\r\n", b''))
+        splittedDecrypt = decrypted.split(b":")
+        eightByte = splittedDecrypt[0]
+        hashOfEight = splittedDecrypt[1]
+        hashOfSPublic = splittedDecrypt[2]
+        print("\n[!] Client's Eight byte key in hash\n")
+        print(hashOfEight)
 
+        # hashing for checking
+        sess = hashlib.md5(eightByte)
+        session = sess.hexdigest()
 
-
-
-
+        hashObj = hashlib.md5(serverPublic)
+        server_public_hash = hashObj.hexdigest()
+        print("server_public_hash :", server_public_hash, "\nhashOfSPublic :", hashOfSPublic, "\nsession :", session,"\nhashOfEight :", hashOfEight)
+        print("\n[!] Matching server's public key & eight byte key\n")
+        if server_public_hash == hashOfSPublic.decode("utf-8") and session == hashOfEight.decode("utf-8"):
+            # encrypt back the eight byte key with the server public key and send it
+            print("\n[!] Sending encrypted session key\n")
+            serverPublic = RSA.importKey(serverPublic).encrypt(eightByte, None)
+            server.send(serverPublic[0])
+            # creating 128 bits key with 16 bytes
+            print("\n[!] Creating AES key\n")
+            key_128 = eightByte + eightByte[::-1]
+            AESKey = AES.new(key_128, AES.MODE_CBC,IV=key_128)
+            # receiving ready
+            serverMsg = server.recv(2048)
+            serverMsg = RemovePadding(AESKey.decrypt(serverMsg))
+            if serverMsg.decode("utf-8") == FLAG_READY:
+                print("\n[!] Server is ready to communicate\n")
+                serverMsg = input("\n[>] ENTER YOUR NAME : ")
+                server.send(bytes(serverMsg, "utf-8"))
+                threading_rec = threading.Thread(target=ReceiveMessage)
+                threading_rec.start()
+                threading_send = threading.Thread(target=SendMessage)
+                threading_send.start()
+        else:
+            print("\nServer (Public key && Public key hash) || (Session key && Hash of Session key) doesn't match")
--- Test2/server.py
+++ Test2/server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jun  3 13:45:16 2020
+Created on Wed Jun  3 13:45:40 2020
 
 @author: augustinjose
 """
@@ -9,51 +9,133 @@ Created on Wed Jun  3 13:45:16 2020
 
 import socket
 import os
+import signal
 import threading
 import hashlib
+import fcntl
+import struct
 from Crypto import Random
 import Crypto.Cipher.AES as AES
 from Crypto.PublicKey import RSA
-import signal
+
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,
+        struct.pack('256s', ifname[:15])
+    )[20:24])
 
 
 def RemovePadding(s):
-    return s.replace('`','')
+    return s.replace(b'`',b'')
 
 
 def Padding(s):
     return s + ((16 - len(s) % 16) * '`')
 
 
-def ReceiveMessage():
+def ConnectionSetup():
     while True:
-        emsg = server.recv(1024)
-        msg = RemovePadding(AESKey.decrypt(emsg))
-        if msg == FLAG_QUIT:
-            print("\n[!] Server was shutdown by admin")
-            os.kill(os.getpid(), signal.SIGKILL)
-        else:
-            print("\n[!] Server's encrypted message \n" + emsg)
-            print("\n[!] SERVER SAID : ", msg)
+        if check is True:
+            client, address = server.accept()
+            print("\n[!] One client is trying to connect...")
+            # get client public key and the hash of it
+            clientPH = client.recv(2048)
+            split = clientPH.split(b":")
+            tmpClientPublic = split[0]
+            clientPublicHash = split[1]
+            print("\n[!] Anonymous client's public key\n")
+            tmpClientPublic = tmpClientPublic.replace(b"\r\n", b'')
+            clientPublicHash = clientPublicHash.replace(b"\r\n", b'')
+            tmpHashObject = hashlib.md5(tmpClientPublic)
+            tmpHash = tmpHashObject.hexdigest()
+
+            if tmpHash == clientPublicHash.decode("utf-8") :
+                # sending public key,encrypted eight byte ,hash of eight byte and server public key hash
+                print("\n[!] Anonymous client's public key and public key hash matched\n")
+                clientPublic = RSA.importKey(tmpClientPublic)
+                fSend = eightByte + b":" + bytes(session, "utf-8") + b":" + bytes(my_hash_public, "utf-8")
+                fSend = clientPublic.encrypt(fSend, None)
+                client.send(fSend[0] + b"\:" + public)
+
+                clientPH = client.recv(2048)
+                if clientPH != b"":
+                    print(clientPH)
+                    clientPH = RSA.importKey(private).decrypt(clientPH)
+                    print("\n[!] Matching session key\n")
+                    if clientPH == eightByte:
+                        # creating 128 bits key with 16 bytes
+                        print("\n[!] Creating AES key\n")
+                        key_128 = eightByte + eightByte[::-1]
+                        AESKey = AES.new(key_128, AES.MODE_CBC,IV=key_128)
+                        clientMsg = AESKey.encrypt(Padding(FLAG_READY))
+                        client.send(clientMsg)
+                        print("\n[!] Waiting for client's name\n")
+                        # client name
+                        clientMsg = client.recv(2048)
+                        CONNECTION_LIST.append((clientMsg, client))
+                        print("\n"+clientMsg.decode("utf-8")+" IS CONNECTED")
+                        threading_client = threading.Thread(target=broadcast_usr,args=[clientMsg,client,AESKey])
+                        threading_client.start()
+                        threading_message = threading.Thread(target=send_message,args=[client,AESKey])
+                        threading_message.start()
+                    else:
+                        print("\nSession key from client does not match")
+            else:
+                print("\nPublic key and public hash doesn't match")
+                client.close()
 
 
-def SendMessage():
+def send_message(socketClient,AESk):
     while True:
-        msg = input("[>] YOUR MESSAGE : ")
-        en = AESKey.encrypt(Padding(msg))
-        server.send(str(en))
+        msg = input("\n[>] ENTER YOUR MESSAGE : ")
+        en = AESk.encrypt(Padding(msg))
+        socketClient.send(en)
         if msg == FLAG_QUIT:
             os.kill(os.getpid(), signal.SIGKILL)
         else:
-            print("\n[!] Your encrypted message \n" + en)
+            print("\n[!] Your encrypted message \n ", en)
+
+
+def broadcast_usr(uname, socketClient,AESk):
+    while True:
+        try:
+            data = socketClient.recv(1024)
+            en = data
+            if data:
+                data = RemovePadding(AESk.decrypt(data))
+                if data == FLAG_QUIT:
+                    print("\n"+uname+" left the conversation")
+                else:
+                    b_usr(socketClient, uname, data)
+                    print("\n[!] ", uname, " SAID : ", data)
+                    print("\n[!] Client's encrypted message\n ",  en)
+        except Exception as x:
+            print(x.message)
+            break
+
+
+def b_usr(cs_sock, sen_name, msg):
+    for client in CONNECTION_LIST:
+        if client[1] != cs_sock:
+            client[1].send(sen_name)
+            client[1].send(msg)
 
 
 if __name__ == "__main__":
-    #objects
+    # objects
+    host = ""
+    port = 0
     server = ""
     AESKey = ""
+    CONNECTION_LIST = []
     FLAG_READY = "Ready"
     FLAG_QUIT = "quit"
+    YES = "1"
+    NO = "2"
+
     # 10.1.236.227
     # public key and private key
     random = Random.new().read
@@ -64,13 +146,9 @@ if __name__ == "__main__":
     tmpPub = hashlib.md5(public)
     my_hash_public = tmpPub.hexdigest()
 
-    #print(public)
-    #print("\n",private)
-
-    host = input("Host : ")
-    port = int(input("Port : "))
-#    host = "127.0.0.1"
-#    port = 5599
+    eightByte = os.urandom(8)
+    sess = hashlib.md5(eightByte)
+    session = sess.hexdigest()
 
     with open('private.txt', 'wb'):
         pass
@@ -81,7 +159,7 @@ if __name__ == "__main__":
         file = open('private.txt', 'wb')
         file.write(private)
         file.close()
-    
+
         file = open('public.txt', 'wb')
         file.write(public)
         file.close()
@@ -89,58 +167,31 @@ if __name__ == "__main__":
         print("Key storing in failed")
 
     check = False
+    print("[1] Auto connect by with broadcast IP & PORT\n[2] Manually enter IP & PORT\n")
+    ask = input("[>] ")
+    if ask == YES:
+        host = get_ip_address('wlp2s0')
+        port = 8080
+    elif ask == NO:
+        host = input("Host : ")
+        port = int(input("Port : "))
+    else:
+        print("[!] Invalid selection")
+        os.kill(os.getpid(), signal.SIGKILL)
 
-    try:
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.bind((host, port))
-        check = True
-    except BaseException:
-        print("\n[!] Check Server Address or Port")
+    print("\n[!] Server IP "+host+" & PORT "+str(port))
 
-    if check is True:
-        print("\n[!] Connection Successful")
-        server.send(public + b':' + bytes(my_hash_public, 'utf-8'))
-        # receive server public key,hash of public,eight byte and hash of eight byte
-        fGet = server.recv(4072)
-        split = fGet.split(":")
-        toDecrypt = split[0]
-        serverPublic = split[1]
-        print("\n[!] Server's public key\n")
-        print(serverPublic)
-        decrypted = RSA.importKey(private).decrypt(eval(toDecrypt.replace("\r\n", '')))
-        splittedDecrypt = decrypted.split(":")
-        eightByte = splittedDecrypt[0]
-        hashOfEight = splittedDecrypt[1]
-        hashOfSPublic = splittedDecrypt[2]
-        print("\n[!] Client's Eight byte key in hash\n")
-        print(hashOfEight)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((host, port))
+    server.listen(1)
+    print("\n[!] Server Connection Successful")
+    check = True
+    # accept clients
+    threading_accept = threading.Thread(target=ConnectionSetup)
+    threading_accept.start()
 
-        # hashing for checking
-        sess = hashlib.md5(eightByte)
-        session = sess.hexdigest()
 
-        hashObj = hashlib.md5(serverPublic)
-        server_public_hash = hashObj.hexdigest()
-        print("\n[!] Matching server's public key & eight byte key\n")
-        if server_public_hash == hashOfSPublic and session == hashOfEight:
-            # encrypt back the eight byte key with the server public key and send it
-            print("\n[!] Sending encrypted session key\n")
-            serverPublic = RSA.importKey(serverPublic).encrypt(eightByte, None)
-            server.send(str(serverPublic))
-            # creating 128 bits key with 16 bytes
-            print("\n[!] Creating AES key\n")
-            key_128 = eightByte + eightByte[::-1]
-            AESKey = AES.new(key_128, AES.MODE_CBC,IV=key_128)
-            # receiving ready
-            serverMsg = server.recv(2048)
-            serverMsg = RemovePadding(AESKey.decrypt(serverMsg))
-            if serverMsg == FLAG_READY:
-                print("\n[!] Server is ready to communicate\n")
-                serverMsg = input("\n[>] ENTER YOUR NAME : ")
-                server.send(serverMsg)
-                threading_rec = threading.Thread(target=ReceiveMessage)
-                threading_rec.start()
-                threading_send = threading.Thread(target=SendMessage)
-                threading_send.start()
-        else:
-            print("\nServer (Public key && Public key hash) || (Session key && Hash of Session key) doesn't match")
+
+
+
